b7d4509a90dc3be6f730a33b3af23d401afbc493
undefined <undefined@domain.tld>
2023-12-07 07:23:29 +0530

    camera works

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -55,11 +56,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+class ComposeFileProvider : FileProvider(R.xml.file_paths) {
+    companion object {
+        fun getImageUri(context: Context): Uri {
+            val directory = File(context.cacheDir, "images")
+            directory.mkdirs()
+
+            val file = File.createTempFile("selected_image_", ".jpg", directory)
+
+            val authority = context.packageName + ".fileProvider"
+            return getUriForFile(context, authority, file)
+        }
+    }
+}
+
 
 @Composable
 fun MyApp() {
+    val context = LocalContext.current
     var hasImage by remember {
         mutableStateOf(false)
+    }
+    var hasPermission by remember {
+        mutableStateOf(true)
     }
     var imageUri by remember {
         mutableStateOf<Uri?>(null)
@@ -69,6 +88,22 @@ fun MyApp() {
         onResult = { uri ->
             hasImage = uri != null
             imageUri = uri
+        }
+    )
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            {
+                hasPermission = isGranted
+            }
+        }
+    )
+
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = { success ->
+            hasImage = success
         }
     )
     Box() {
@@ -101,7 +136,16 @@ fun MyApp() {
                 Button(onClick = { imagePicker.launch("image/*") }) {
                     Text(text = "Select Image")
                 }
-                Button(onClick = { /*TODO*/ }) {
+                Button(onClick = {
+                    if (hasPermission){
+                        val uri = ComposeFileProvider.getImageUri(context)
+                        imageUri = uri
+                        hasImage = false
+                        cameraLauncher.launch(uri)
+                    } else {
+                        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    }
+                }) {
                     Text(text = "Take Picture")
                 }
 
